96014fff91883c58046e661ff903f623a76300dc
rafaelfelipelipe <rafaelfelipelipe0@gmail.com>
2024-07-20 23:12:13 -0300

    Primeiro commit

---
+++ Js/script.js
@@ -0,0 +1,45 @@
+const resultado = document.getElementById('resultado')
+
+function userPlay(userchoice){
+    const computerchoice = getcomputerchoice();
+    const gameresult = getGameResult(userchoice, computerchoice);
+    resultado.innerHTML = `você escolheu  ${userchoice}<br>Computador escolheu: ${computerchoice}.<br> ${gameresult}`;
+
+}
+
+ function getcomputerchoice(){
+    const choices = ['pedra', 'papel', 'tesoura'];
+    const randomIndex = Math.floor(Math.random()*3);
+    return choices[randomIndex];
+
+ }
+
+ function getGameResult(userchoice, computerchoice){
+
+    if(userchoice === computerchoice){
+        return 'Empate';
+
+    }
+
+    if( 
+        
+        (userchoice === "pedra" && computerchoice === "tesoura") || 
+        (userchoice === "papel" && computerchoice === "pedra") || 
+        (userchoice === "tesoura" && computerchoice === "papel")        
+
+    )        
+        
+    {
+
+        return "Você ganhou!";
+
+    }
+
+
+        return "Você perdeu!";
+
+ }
+
+
+
+ 
